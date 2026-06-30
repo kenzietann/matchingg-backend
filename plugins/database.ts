8007644c42c5fcba218980a75314eeeee9beb003
kenkzuha <kenkzuha@gmail.com>
2026-06-30 11:18:06 +0900

    fix: move connection string to env variable

--- plugins/database.ts
+++ plugins/database.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config'
 import fastifyPostgres from '@fastify/postgres';
 import fastifyPlugin from 'fastify-plugin';
 import { FastifyInstance } from 'fastify';
 
 async function dbConnector(fastify: FastifyInstance){
   fastify.register(fastifyPostgres, {
-    connectionString: 'postgres://kenkzuha@localhost/matchingg'
+    connectionString: process.env.DB_CONNECTION
   })
 }
 
