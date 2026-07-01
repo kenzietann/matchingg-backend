06b5b84eda8689c6b14c5a7df2578a1db33b2a41
kenkzuha <kenkzuha@gmail.com>
2026-07-01 13:17:31 +0900

    feat(database): initialize Fastify TypeORM

---
+++ entities/user/user.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column("varchar") // Explicitly set column type because TSX ignored my tsconfig and caused a runtime error
+  name!: string;
+}
