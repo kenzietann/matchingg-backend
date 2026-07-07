436bad1b356d21cf46d028e039904a1abd226c6b
kenkzuha <kenkzuha@gmail.com>
2026-07-07 23:34:58 +0900

    chore: remove user folder from entities folder

--- plugins/database.ts
+++ plugins/database.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import { FastifyInstance } from 'fastify';
 import dbConnection from 'typeorm-fastify-plugin';
 import { UserEntity } from '../entities/user/user.entity.js';
+import fastifyPostgres from '@fastify/postgres';
 
 export async function dbConnector(fastify: FastifyInstance){
   fastify.register(fastifyPostgres, {
