c90983d94e8cf3fdce690bd66bc07dec2270eaa5
kenkzuha <tandera.kenzie@gmail.com>
2026-07-25 23:06:41 +0900

    feat: Set up TypeORM migrations

--- plugins/database.ts
+++ plugins/database.ts
@@ -5,6 +5,11 @@ import { DataSource } from 'typeorm';
 import { UserEntity } from '../core/entities/user.entity.js';
 import { ResultsEntity } from '../core/entities/results.entity.js';
 import { env } from '../core/env.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -12,6 +17,7 @@ export const AppDataSource = new DataSource({
   synchronize: false,
   logging: false,
   entities: [UserEntity, ResultsEntity],
+  migrations: [join(__dirname, '../migrations/**/*{.js,.ts}')]
 });
 
 export async function dbConnector(fastify: FastifyInstance) {
